96a201608f654f4ef8e7549fb481f656d2601618
Lawrence Hemsley <lawrence.hemsley@gmail.com>
2020-04-24 16:30:19 -0600

    renamed source/app.d to source/zen.d

--- source/zen.d
+++ source/zen.d
@@ -1,30 +1,29 @@
-/*
- * app.d
- *
- * This file is a part of the ZenEditor package.
- * Zen is an editor for the D language.
- *
- * Copyright (c) 2018 Lawrence Hemsley <lawrence.hemsley@gmail.com>
- * All Rights Reserved
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>..
- *
- * ZenEditor is based in part on works of the GtkD project (http://gtkd.org).
- *
- */
-
 module zen;
+
+/*
+This file is a part of the Zen Editor package.
+Zen is an editor for the D language.
+
+Copyright (c) 2018 Lawrence Hemsley <lawrence.hemsley@gmail.com>
+All Rights Reserved
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>..
+
+Zen Editor is based in part on works of the GtkD project (http://gtkd.org).
+*/
+
+
 
 //debug = 1;
 
